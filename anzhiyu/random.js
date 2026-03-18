0022bb17c9bbeb1f85b54ae2a71e819917b6e530
ZQH232 <2826566773@qq.com>
2026-03-18 22:28:11 +0800

    Site updated: 2026-03-18 22:28:01

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/03/18/32/","2023/08/10/C语言/","2026/02/05/复试回答加口试/","2023/08/01/复试/","2023/08/01/简历回答/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
